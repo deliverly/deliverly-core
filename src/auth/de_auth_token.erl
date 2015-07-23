dba41698dda2e7beef4c674f06bfc3a9cc7c1464
silkbottle <silkbottle@gmail.com>
2015-07-23 18:24:33 +0400

    add support of request with several tokens

--- src/auth/de_auth_token.erl
+++ src/auth/de_auth_token.erl
@@ -15,9 +15,15 @@
 request_token(Options) ->
   random:seed(erlang:now()),
   NewOptions = merge(Options),
+  {Params, Tokens} = request_token_opt(NewOptions),
+  NewParams =
+    case NewOptions of
+      #{scope := <<"*">>} -> Params;
+      #{scope := Apps} -> [{scope, Apps} | Params]
+    end,
   case NewOptions of
-    #{scope := <<"*">>} -> request_token_opt(NewOptions);
-    #{scope := Apps} -> [{scope, Apps} | request_token_opt(NewOptions)]
+    #{count := undefined} -> [{token, hd(Tokens)} | NewParams];
+    _ -> [{tokens, Tokens} | NewParams]
   end.
 
 %% @doc
@@ -46,6 +52,7 @@ verify(Client, Data, _Args) ->
 
 merge(Options) ->
   Default = #{
+    count => undefined,
     expires_in => ulitos_app:get_var(deliverly, token_timeout, 60),
     once => ulitos_app:get_var(deliverly, token_once, false),
     scope => <<"*">>
@@ -76,6 +83,9 @@ to_term(expires_in, <<"infinity">>) ->
 to_term(expires_in, Timeout) when is_binary(Timeout) ->
   binary_to_integer(Timeout);
 
+to_term(count, Count) when is_binary(Count) ->
+  max(min(binary_to_integer(Count), ?Config(max_tokens, 100)), 1);
+
 to_term(once, <<"true">>) ->
   true;
 
@@ -87,29 +97,47 @@ to_term(_Key, Value) ->
 
 -spec request_token_opt(map()) -> proplists:proplist().
 
-request_token_opt(#{expires_in := infinity, once := true, scope := Apps}) ->
-  Token = list_to_binary(ulitos:random_string(8)),
-  Namespace = ?Config(redis_namespace, <<"">>),
-  redis_cli:q(["SET", <<Namespace/binary, Token/binary>>, Apps]),
-  [{token, Token}, {once, true}];
+request_token_opt(#{expires_in := infinity, once := true, scope := Apps, count := Count}) ->
+  Tokens = tokens(infinity, true, Apps, Count),
+  {[{once, true}], Tokens};
 
-request_token_opt(#{expires_in := infinity, once := false, scope := Apps}) ->
-  Token = list_to_binary(ulitos:random_string(9)),
-  Namespace = ?Config(redis_namespace, <<"">>),
-  redis_cli:q(["SET", <<Namespace/binary, Token/binary>>, Apps]),
-  [{token, Token}];
+request_token_opt(#{expires_in := infinity, once := false, scope := Apps, count := Count}) ->
+  Tokens = tokens(infinity, false, Apps, Count),
+  {[], Tokens};
 
-request_token_opt(#{expires_in := Timeout, once := false, scope := Apps}) ->
-  Token = list_to_binary(ulitos:random_string(9)),
-  Namespace = ?Config(redis_namespace, <<"">>),
-  redis_cli:q(["SETEX", <<Namespace/binary, Token/binary>>, Timeout, Apps]),
-  [{token, Token}, {expires_in, Timeout}];
+request_token_opt(#{expires_in := Timeout, once := false, scope := Apps, count := Count}) ->
+  Tokens = tokens(Timeout, false, Apps, Count),
+  {[{expires_in, Timeout}], Tokens};
 
-request_token_opt(#{expires_in := Timeout, once := true, scope := Apps}) ->
-  Token = list_to_binary(ulitos:random_string(8)),
+request_token_opt(#{expires_in := Timeout, once := true, scope := Apps, count := Count}) ->
+  Tokens = tokens(Timeout, true, Apps, Count),
+  {[{expires_in, Timeout}, {once, true}], Tokens}.
+
+-spec token(infinity | non_neg_integer(), boolean(), binary()) -> binary().
+
+token(Timeout, Once, Apps) ->
+  Length = 9 - if Once -> 1; true -> 0 end,
+  Token = list_to_binary(ulitos:random_string(Length)),
   Namespace = ?Config(redis_namespace, <<"">>),
-  redis_cli:q(["SETEX", <<Namespace/binary, Token/binary>>, Timeout, Apps]),
-  [{token, Token}, {expires_in, Timeout}, {once, true}].
+  case Timeout of
+    infinity ->
+      redis_cli:q(["SET", <<Namespace/binary, Token/binary>>, Apps]);
+    _ ->
+      redis_cli:q(["SETEX", <<Namespace/binary, Token/binary>>, Timeout, Apps])
+  end,
+  Token.
+
+-spec tokens(infinity | non_neg_integer(), boolean(), binary(), undefined | non_neg_integer())
+      -> [binary()].
+
+tokens(Timeout, Once, Apps, undefined) ->
+  [token(Timeout, Once, Apps)];
+
+tokens(Timeout, Once, Apps, Count) ->
+  redis_cli:q(["MULTI"]),
+  Tokens = [token(Timeout, Once, Apps) || _ <- lists:seq(1, Count)],
+  redis_cli:q(["EXEC"]),
+  Tokens.
 
 -spec get_scope(binary()) -> undefined | binary().
 
